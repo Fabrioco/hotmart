5833b9392e4069cdff8865ba34a5a84664f3ceb1
Fabrioco008 <fabriciooliveiralopes50@gmail.com>
2024-12-14 19:18:19 -0300

    :sparkles: feat: Componente para mandar notificação

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,9 +1,11 @@
+import { SendNotification } from "./components/SendNotification";
 import { UploadVideo } from "./components/UploadVideo";
 
 export default function Admin() {
   return (
     <div className="w-full h-full rounded-md flex justify-between items-center flex-wrap gap-5">
       <UploadVideo />
-    </div>  
+      <SendNotification />
+    </div>
   );
 }
