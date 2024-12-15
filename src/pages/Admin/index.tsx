ce47dbe504dd498aab7b248ed5546dd45ee9058b
Fabrioco008 <fabriciooliveiralopes50@gmail.com>
2024-12-14 21:59:38 -0300

    :sparkles: feat: Sistema de excluir o curso

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,13 +1,15 @@
 import { Discount } from "./components/Discount";
 import { SendNotification } from "./components/SendNotification";
+import { SettingsCourse } from "./components/SettingsCourse";
 import { UploadVideo } from "./components/UploadVideo";
 
 export default function Admin() {
   return (
-    <div className="w-full h-full rounded-md flex justify-between items-center flex-wrap gap-5">
+    <div className="w-full h-full rounded-md flex flex-col justify-between items-center flex-wrap gap-5">
       <UploadVideo />
       <SendNotification />
       <Discount />
+      <SettingsCourse />
     </div>
   );
 }
