8abf36f95d053a469acabea5ddc88a027b73eeda
Fabrioco008 <fabriciooliveiralopes50@gmail.com>
2024-12-28 17:59:45 -0300

    :sparkles: feat: Mostrar o nome e foto do administrador

--- src/pages/Messages/index.tsx
+++ src/pages/Messages/index.tsx
@@ -1,6 +1,7 @@
 import { collection, getDocs, orderBy, query } from "firebase/firestore";
 import React from "react";
 import { db } from "../../services/firebaseConnection";
+import { UserDataProps } from "../../contexts/userDataContext";
 
 type Message = {
   title: string;
@@ -11,6 +12,7 @@ type Message = {
 
 export default function Messages() {
   const [messages, setMessages] = React.useState<Message[]>([]);
+  const [admin, setAdmin] = React.useState<UserDataProps>();
 
   const loadMessages = async () => {
     const q = query(collection(db, "notifications"), orderBy("date", "asc"));
@@ -24,18 +26,31 @@ export default function Messages() {
 
   React.useEffect(() => {
     loadMessages();
+    findAdmin();
   }, []);
+
+  const findAdmin = async () => {
+    const docSnap = await getDocs(collection(db, "users"));
+
+    if (docSnap.docs) {
+      const allUsers = docSnap.docs.map((doc) => doc.data()) as UserDataProps[];
+
+      const admin = allUsers.find((user) => user.isAdmin === true);
+
+      setAdmin(admin);
+    }
+  };
 
   return (
     <div className="w-full h-full flex flex-row  bg-white rounded-2xl overflow-x-hidden">
       <div className="w-full h-full rounded-br-2xl overflow-y-hidden">
         <div className="w-full h-32 bg-white rounded-tr-2xl flex items-center border-b border-gray-300">
           <img
-            src="https://via.placeholder.com/150"
-            alt="foto do professor"
+            src={admin?.profilePhoto}
+            alt={admin?.name}
             className="p-4 rounded-full h-full w-auto"
           />
-          <p className="text-2xl font-secondary">Nome do professor</p>
+          <p className="text-2xl font-secondary">{admin?.name}</p>
         </div>
         <div className="w-full h-full bg-gray-50 p-4 overflow-y-auto">
           {messages.map((message) => (
