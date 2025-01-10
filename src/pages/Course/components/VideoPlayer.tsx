14b68a3c044b0cf6729fce23465eadcb54d786f8
Fabrioco008 <fabriciooliveiralopes50@gmail.com>
2025-01-10 00:13:13 -0300

    commit apenas para ter o acesso no outro computador

--- src/pages/Course/components/VideoPlayer.tsx
+++ src/pages/Course/components/VideoPlayer.tsx
@@ -7,46 +7,50 @@ import { db } from "../../../services/firebaseConnection";
 import { doc, getDoc } from "firebase/firestore";
 
 export const VideoPlayer = () => {
-  const [courseData, setCourseData] = React.useState<Course[]>([]);
-
+  const [courseData, setCourseData] = React.useState<Course | null>(null);
   const { course } = useParams();
 
   React.useEffect(() => {
     const fetchVideo = async () => {
       try {
-        const docSnap = await getDoc(doc(db, "courses", course as string));
+        if (!course) {
+          console.error("Curso não especificado.");
+          return;
+        }
+
+        const docRef = doc(db, "courses", course);
+        const docSnap = await getDoc(docRef);
 
         if (docSnap.exists()) {
-          setCourseData([docSnap.data() as Course]);
+          setCourseData(docSnap.data() as Course);
+        } else {
+          console.warn("Nenhum curso encontrado para o ID especificado.");
         }
       } catch (error) {
-        console.log(error);
+        console.error("Erro ao buscar dados do curso:", error);
       }
     };
+
     fetchVideo();
-  }, []);
+  }, [course]);
+
+  if (!courseData) {
+    return (
+      <div className="w-full h-full flex justify-center items-center">
+        <p>Carregando informações do curso...</p>
+      </div>
+    );
+  }
 
   return (
     <div className="w-full h-full flex flex-col overflow-y-auto font-secondary">
       <div className="relative w-full h-auto">
         <ReactPlayer
-          url={courseData[0]?.link}
+          url={courseData.link}
           height="700px"
           width="100%"
           onEnded={() => alert("O vídeo terminou")}
-          config={{
-            youtube: {
-              playerVars: {
-                autoplay: 0,
-                controls: 1,
-                modestbranding: 1,
-                rel: 0,
-                showinfo: 0,
-                fs: 1,
-                iv_load_policy: 3,
-              },
-            },
-          }}
+          controls={true}
         />
       </div>
 
@@ -55,14 +59,14 @@ export const VideoPlayer = () => {
           Visão Geral
         </h1>
         <div className="text-2xl mt-2 p-2">
-          <h1 className="text-2xl">{courseData[0]?.title}</h1>
+          <h1 className="text-2xl">{courseData.title}</h1>
           <h2 className="text-xl text-gray-600 cursor-pointer">
             Fabrício Oliveira Lopes
           </h2>
         </div>
         <div className="mt-4 p-2">
           <p className="text-xl uppercase">Sobre o curso</p>
-          <p className="mt-1 w-10/12">{courseData[0]?.desc}</p>
+          <p className="mt-1 w-10/12">{courseData.desc}</p>
         </div>
       </div>
     </div>
